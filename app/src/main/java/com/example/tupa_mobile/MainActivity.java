61b4ca5edb6308f9e505cf4a6d6d44fb08da9bc4
Chigutti <80776666+Chigutti@users.noreply.github.com>
2021-04-03 13:31:30 -0300

    Coloquei uma navegação teste

--- app/src/main/java/com/example/tupa_mobile/MainActivity.java
+++ app/src/main/java/com/example/tupa_mobile/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.tupa_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottonNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottonNav = findViewById(R.id.bottomNavigationView);
+        bottonNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayout, new MapFragment()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.item1:
+                    selectedFragment = new MapFragment();
+                break;
+
+                case R.id.item2:
+                    selectedFragment = new ForecastFragment();
+                break;
+
+                case R.id.item3:
+                    selectedFragment = new ConfigFragment();
+                break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayout, selectedFragment).commit();
+            return true;
+
+        }
+    };
 }
