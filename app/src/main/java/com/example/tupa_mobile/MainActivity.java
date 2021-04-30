86a04679c4ccff6654ccb55b1eaca7373624ebdb
Chigutti <80776666+Chigutti@users.noreply.github.com>
2021-04-30 18:02:29 -0300

    Adicionei os icones, refiz a toolbar, adicionei a atividade de notificações e removi a opção de notificações da navegação inferior

--- app/src/main/java/com/example/tupa_mobile/MainActivity.java
+++ app/src/main/java/com/example/tupa_mobile/MainActivity.java
@@ -5,8 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,7 +15,6 @@ import com.example.tupa_mobile.Fragments.SettingsFragment;
 import com.example.tupa_mobile.Fragments.ForecastFragment;
 import com.example.tupa_mobile.Fragments.HistoryFragment;
 import com.example.tupa_mobile.Fragments.MapFragment;
-import com.example.tupa_mobile.Fragments.NotificationFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,25 +29,40 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNav = findViewById(R.id.bottomNavigationView);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+        toolbar = findViewById(R.id.mapToolbar);
+        setSupportActionBar(toolbar);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayout, new MapFragment()).commit();
-
-        toolbar = findViewById(R.id.mainToolBar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Tupã");
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-
-        getMenuInflater().inflate(R.menu.secondary_menu, menu);
-
+        getMenuInflater().inflate(R.menu.map_menu, menu);
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
 
+            case R.id.notificationItem:
+                startNotificationActivity();
+                return true;
+
+            case R.id.markerItem:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void startNotificationActivity() {
+        Intent intent = new Intent(this, NotificationActivity.class);
+        startActivity(intent);
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -68,10 +83,6 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
                 case R.id.item4:
-                    selectedFragment = new NotificationFragment();
-                    break;
-
-                case R.id.item5:
                     selectedFragment = new SettingsFragment();
                     break;
             }
---
+++ app/src/main/java/com/example/tupa_mobile/NotificationActivity.java
@@ -0,0 +1,31 @@
+package com.example.tupa_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+        toolbar = findViewById(R.id.notificationToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.arrow_right_icon_black_white_theme_small);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.notification_menu, menu);
+        return true;
+    }
+}
